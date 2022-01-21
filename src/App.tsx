35b42844b221e9b19c15c7a4a3df86db47e8de4c
rbrignall <rbrignall@gmail.com>
2022-01-21 21:22:44 +0000

    npm install post build

--- src/App.tsx
+++ src/App.tsx
@@ -109,7 +109,7 @@ function App() {
         variant="success"
       />
       <div className="flex w-80 mx-auto items-center mb-8">
-        <h1 className="text-xl grow font-bold">Not Wordle</h1>
+        <h1 className="text-xl grow font-bold">Numble</h1>
         <InformationCircleIcon
           className="h-6 w-6 cursor-pointer"
           onClick={() => setIsInfoModalOpen(true)}
